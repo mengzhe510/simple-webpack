24b5b96b1cf2bece92ec6a6215466b5dfb0027b7
宋孟哲 <mengzhe.song@ly.com>
2020-08-31 17:44:49 +0800

    自己写一遍

--- bundle.js
+++ bundle.js
@@ -1,4 +1,4 @@
 // !读取webpack的配置
 const options = require('./webpack.config.js')
-const Webpack = require('./lib/webpack.js');
+const Webpack = require('./lib/webpack-test-1.js');
 new Webpack(options).run()
